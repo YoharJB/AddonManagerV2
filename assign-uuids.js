a6f7f3deaefe1e01476d9f1aed335722c1550b92
YoharJB <yoharjb@gmail.com>
2024-10-05 16:55:58 -0400

    Update assign-uuids.js

--- assign-uuids.js
+++ assign-uuids.js
@@ -1,47 +1,29 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-// Lista de archivos JSON a procesar con sus rutas
-const jsonFiles = [
-  'LK/lichking.json',
-  'Cata/cataclysm.json',
-  'Panda/pandaria.json'
-];
+const files = ['LK/lichking.json', 'Cata/cataclysm.json', 'Panda/pandaria.json'];
 
-let anyUpdates = false; // Bandera para verificar si se realizaron cambios en algún archivo
-
-jsonFiles.forEach(filePath => {
+files.forEach(file => {
   try {
-    // Leer y parsear el archivo JSON
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
-    // Verificar y asignar UUIDs donde sea necesario
+    const data = JSON.parse(fs.readFileSync(file, 'utf-8'));
     let updated = false;
-    const updatedData = data.map(item => {
-      if (!item.uuid) {
+
+    const updatedData = data.map(obj => {
+      if (!obj.uuid) {
         updated = true;
-        return { ...item, uuid: uuidv4() };
+        return { ...obj, uuid: uuidv4() };
       }
-      return item;
+      return obj;
     });
 
-    // Si se realizaron cambios, escribir el archivo actualizado
     if (updated) {
-      fs.writeFileSync(filePath, JSON.stringify(updatedData, null, 2));
-      console.log(`UUIDs asignados en ${filePath}.`);
-      anyUpdates = true;
+      fs.writeFileSync(file, JSON.stringify(updatedData, null, 2));
+      console.log(`UUIDs asignados en ${file}.`);
     } else {
-      console.log(`Todos los objetos en ${filePath} ya tienen un UUID. No se realizaron cambios.`);
+      console.log(`Todos los objetos en ${file} ya tienen un UUID.`);
     }
   } catch (err) {
-    console.error(`Error al procesar ${filePath}:`, err);
+    console.error(`Error al procesar ${file}:`, err);
     process.exit(1);
   }
 });
-
-// Salida final si se realizaron actualizaciones
-if (anyUpdates) {
-  console.log('Se realizaron actualizaciones en los archivos JSON.');
-} else {
-  console.log('No se realizaron cambios en ninguno de los archivos JSON.');
-}
